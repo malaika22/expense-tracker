a74d52c3911c83ae3503968e789713503cf4d848
malaika22 <malaikaafridi22@gmail.com>
2020-10-24 19:45:52 +0500

    Intial Balance Transaction Header Components

---
+++ src/components/Balance.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Balance(){
+    return(
+        <div>
+            <h3>Balance: $40</h3>
+        </div>
+    )
+}
---
+++ src/components/Transaction.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+
+ const Transaction = () => {
+    return(
+        <h2>Transaction Form</h2>
+    )
+}
+
+
+export default Transaction;
